f12f954eb0a9ef9b4c9580440670e36aa6d62e13
maciel <macielgaleano.jh@gmail.com>
2020-08-21 15:33:04 -0300

    A big power need a big responsability

---
+++ JQuery UI/js/jquery_main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    //Mover elemento por la pagina
+    $('.element').draggable();
+
+    $('#resizable').resizable();
+
+    //Elemts selectables
+    $('.list').selectable();
+});
