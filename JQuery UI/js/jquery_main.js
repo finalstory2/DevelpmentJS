8fd2741e63369fa5edb9b9114725272bcec48674
maciel <macielgaleano.jh@gmail.com>
2020-08-23 00:09:11 -0300

    Dropeable items

--- JQuery UI/js/jquery_main.js
+++ JQuery UI/js/jquery_main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
     //Mover elemento por la pagina
     $('.element').draggable();
 
@@ -11,6 +13,13 @@ $(document).ready(function() {
             console.log('List changed');
             console.log(event + '_' + ui);
             console.log(event);
+        }
+    });
+    $('#moved_elemnt').draggable();
+    $('#area').droppable({
+        drop: function() {
+            console.log("Has soltado algo dentro");
+            $('#moved_elemnt').css('display', 'none');
         }
     });
 });
