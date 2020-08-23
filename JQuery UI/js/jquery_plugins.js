3fef37873398d4ea1dba24f209bc2df834122ec0
maciel <macielgaleano.jh@gmail.com>
2020-08-23 18:56:33 -0300

    Fineshed testes with plugins and widgets, I add boostrap is better.

---
+++ JQuery UI/js/jquery_plugins.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    //tooltip
+    $(document).tooltip();
+    //dialog
+
+    $('#btn_see_popup').click(function() {
+        $('#popup').dialog();
+    });
+
+    //date picker
+    $('#calendar').datepicker();
+});
