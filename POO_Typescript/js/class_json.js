ede594992d18bdd5f1bc0581864312c3466321e4
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-08-27 22:51:09 -0300

    Creating clases in JS

--- POO_Typescript/js/class_json.js
+++ POO_Typescript/js/class_json.js
@@ -1,3 +1,16 @@
-window.addEventListener('load', (e) = {
-    console.log('ni idea');
+window.addEventListener('load', () => {
+    var skate = {
+        color: 'red',
+        model: 'Santa cruz',
+        wheels: 'Vortize',
+        tricks: 'Steam',
+        changeColor: function(new_color) {
+            // skate.color = new_color;
+            this.color = new_color;
+        }
+    }
+
+    console.log(skate);
+    skate.changeColor("blue");
+    console.log(skate);
 });
