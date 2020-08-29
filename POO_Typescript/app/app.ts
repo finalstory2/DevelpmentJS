87a5850c06888030d2a01d323ac99d2bdc1a8d74
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-08-28 23:44:35 -0300

    seeing the datatypes in typescript

--- POO_Typescript/app/app.ts
+++ POO_Typescript/app/app.ts
@@ -1,1 +1,31 @@
 let nombre :string = "felipe";
+
+//letibales and datatypes
+
+let chain: string = 'chain';
+let number: number = 10;
+let boolean: boolean = true;
+
+//any letiable
+
+let any: any = 'hello';
+
+//Aray definition
+
+let array_type_string: Array<string> = ['value1', 'value2', 'value3'];
+let array_type_number: Array<number> = [1, 2, 3];
+let array_type_any: Array<any> = [1, "2", true];
+let array_type_array_any: Array<any> = [array_type_string, array_type_number, array_type_any];
+
+console.log(array_type_array_any);
+
+//Conditional datetype
+
+let String_number: number | string = 'red';
+String_number = 10;
+
+//Creating new dataType
+
+type letter_numbers = string | number;
+
+let letter_number: letter_numbers = 10;
--- POO_Typescript/js/dist/app.dev.js
+++ POO_Typescript/js/dist/app.dev.js
@@ -1,4 +1,3 @@
 "use strict";
 
-var nombre = "felipe";
-console.log(nombre);
+var nombre = "felipe"; //Varibales and datatypes
